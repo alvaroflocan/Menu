cc0f7f6061371a899c09ace3470e1d61cde25ef4
alvaroflo <alvaro.flocan@educa.jcyl.es>
2023-11-01 10:07:46 +0100

    Menu funcionando

--- app/src/main/java/com/example/menu/MainActivity.kt
+++ app/src/main/java/com/example/menu/MainActivity.kt
@@ -4,32 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
 
 //https://codersguidebook.com/how-to-create-an-android-app/create-a-menu
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.mytollbar))
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_main, menu)
+        getMenuInflater().inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle item selection
-        return when (item.itemId) {
-            /*R.id.new_game -> {
-                newGame()
-                true
-            }
-            R.id.help -> {
-                showHelp()
-                true
-            }*/
-            else -> super.onOptionsItemSelected(item)
+         when (item.itemId) {
+            R.id.menu_ajustes -> Toast.makeText(this,"Ajustes",Toast.LENGTH_SHORT).show()
+             R.id.menu_inicio -> Toast.makeText(this,"Inicio",Toast.LENGTH_SHORT).show()
+             R.id.menu_perfil -> Toast.makeText(this,"Perfil",Toast.LENGTH_SHORT).show()
+            R.id.menu_guardar -> Toast.makeText(this,"Guardado",Toast.LENGTH_LONG).show()
+
+         }
+            return super.onOptionsItemSelected(item)
         }
-    }
+
 }
